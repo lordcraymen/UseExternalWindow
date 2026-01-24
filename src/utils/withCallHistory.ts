9e181977ad8b6b325d231aabd737a0c997405d39
lordcraymen <florian.patzke@t-online.de>
2026-01-24 17:03:55 +0100

    Refactor withCallHistory to simplify implementation and remove unused overloads

--- src/utils/withCallHistory.ts
+++ src/utils/withCallHistory.ts
@@ -1,42 +1,15 @@
-type AnyFn = (...args: any[]) => any;
-
-type WithCallHistory<F extends AnyFn, StoreKey extends PropertyKey, Entry> =
-  F & { [K in StoreKey]: Entry[] };
-
-// Overload 1: no transform -> entries are exactly the original args tuple
-function withCallHistory<
-  F extends AnyFn,
-  StoreKey extends PropertyKey = "callHistory"
->(
+function withCallHistory<F extends (...args: any[]) => any>(
   fn: F,
-  storeName?: StoreKey
-): WithCallHistory<F, StoreKey, Parameters<F>>;
-
-// Overload 2: with transform -> entries are whatever transform returns
-function withCallHistory<
-  F extends AnyFn,
-  StoreKey extends PropertyKey = "callHistory",
-  Entry = unknown
->(
-  fn: F,
-  storeName: StoreKey | undefined,
-  beforeStorage: (args: Parameters<F>) => Entry
-): WithCallHistory<F, StoreKey, Entry>;
-
-// Implementation
-function withCallHistory(
-  fn: AnyFn,
-  storeName: PropertyKey = "callHistory",
-  beforeStorage?: (args: any[]) => unknown
+  Store: typeof Array = Array,
 ) {
-  const wrapped = ((...args: any[]) => {
-    const entry = beforeStorage ? beforeStorage(args) : args;
-    (wrapped as any)[storeName].push(entry);
+  const store = new Store<Parameters<F>>();
+  
+  const wrapped = ((...args: Parameters<F>) => {
+    store.push(args);
     return fn(...args);
-  }) as any;
+  }) as F;
 
-  wrapped[storeName] = [];
-  return wrapped;
+  return [wrapped, store]
 }
 
 export { withCallHistory };
